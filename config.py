70aec416effafee370cbda6b0b84200f3ecb272b
Tyler Weaver <tylerjw@gmail.com>
2017-02-08 19:44:05 -0700

    config file handling

---
+++ config.py
@@ -0,0 +1,104 @@
+import configparser
+import os, sys
+import getpass
+
+def enter_password():
+    pprompt = lambda: (getpass.getpass(), getpass.getpass('Retype password: '))
+    while True:
+        p1, p2 = pprompt()
+        if p1 != p2:
+            print('Passwords do not match. Try again')
+        elif p1 == '':
+            print('Enter a password, cannot be blank. Try again')
+        else:
+            break
+    return p1
+
+def enter_hostname():
+    host = 'localhost'
+    try:
+        host = input('Enter the hostname or ip of server (localhost): ') or 'localhost'
+    except:
+        pass
+    return host
+
+def enter_port():
+    port = 0
+    while not port:
+        try:
+            portstr = input('Enter the port number for communication with server (12345): ') or '12345'
+        except:
+            pass
+
+        try:
+            port = int(portstr)
+        except:
+            print('Hey you dullard, port NUMBER is a number no whatever {} is'.format(portstr))
+    return str(port)
+
+def get_config(configpath = None):
+    if not configpath:
+        configpath = os.path.abspath('.blackjay/config')
+
+    # assumes we are in the base directory
+    config = configparser.ConfigParser()
+
+    print('Looking for blackjay in {}'.format(os.path.abspath('.')))
+
+    if os.path.isdir('.blackjay') is not True:
+        print('Looks like a new install... creating a .blackjay folder')
+        os.mkdir('.blackjay')
+
+    if os.path.exists(configpath) is not True:
+        print('Setting up a new config file at {}'.format(configpath))
+        config.add_section('blackjay')
+        config['blackjay']['host'] = enter_hostname()
+        config['blackjay']['port'] = enter_port()
+        config['blackjay']['password'] = enter_password()
+
+        with open(configpath, 'w') as configfile:
+            config.write(configfile)
+
+    with open(configpath, 'r') as configfile:
+        config.read_file(configfile)
+
+    if not 'blackjay' in config.sections():
+        print('It looks like your config file is missint the blackjay section')
+        config.add_section('blackjay')
+
+    # we now test if all the options have been set in config
+    host = config.get('blackjay','host',fallback=None)
+    portstr = config.get('blackjay','port',fallback=None)
+    password = config.get('blackjay','password',fallback=None)
+
+    if not host:
+        print('Host not found in config, enter new host value.')
+        config['blackjay']['host'] = enter_hostname()
+
+    if not portstr:
+        print('Port not found in config, enter new port value')
+        config['blackjay']['port'] = enter_port()
+
+    if not password:
+        print('Password not found in config, enter new password value')
+        config['blackjay']['port'] = enter_password()
+
+    with open(configpath, 'w') as configfile:
+        config.write(configfile)
+        
+    return dict(config.items('blackjay'))
+
+
+def main():
+    if len(sys.argv) == 2:
+        os.chdir(sys.argv[1])
+
+    config = get_config()
+    print('Host: ', config['host'])
+    print('Port: ', config['port'])
+    print('Password: ', config['password'])
+
+    print(config)
+
+if __name__ == '__main__':
+    main()
