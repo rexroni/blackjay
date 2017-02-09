a734454dfb917b5cb1783ddcb8f0a37790ca3de1
Tyler Weaver <tylerjw@gmail.com>
2017-02-08 21:35:45 -0700

    refactor to refactor it all

---
+++ networking.py
@@ -0,0 +1,98 @@
+import socket
+import os
+
+metadata_req_message = b"gimme the metadata"
+prepare_message = b"prepare to receive all my acorns"
+prepare_response = b"send your worst"
+
+def recv_all(sock):
+    total_len = 0; payload_data = b''; size = 0
+    size_data = sock_data = b''
+    len_data = b''; len_size = 0
+    # get the length field size
+    while not len_size:
+        sock_data = sock.recv(1)
+        if len(sock_data)>0:
+            len_size = sock_data[0]
+
+    # get the length field
+    while len(len_data) < len_size:
+        len_data += sock.recv(len_size-len(len_data))
+        if len(len_data) == len_size:
+            size = int(len_data[:len_size])
+
+    # get the data
+    while len(payload_data) < size:
+        payload_data += sock.recv(min(size-total_len, 2048))
+
+    return payload_data
+
+def send_file(filename, sock):
+    size = str(os.stat(filename).st_size)
+    print("send_file: file size: {}".format(size), flush=True)
+    with open(filename, 'rb') as f:
+        send_size(size, sock)
+        data = f.read(2048)
+        while data:
+            send_size(data, sock)
+            data = f.read(2048)
+
+def progress_bar(completed, size):
+    width = 60
+    progress = completed/size
+    progress_bar_width = width - 13
+    num_equals = int(progress*progress_bar_width)
+    num_space = int(progress_bar_width - num_equals)
+    progress_bar = "="*num_equals + " "*num_space
+    print("\r[{}] {:.2f}%".format(progress_bar, (progress*100)), end='')
+    if completed == size:
+        print("")
+
+def recv_file(filename, sock):
+    size = int(recv_all(sock))
+    print("recv_file: {}, size: {}".format(filename,size), flush=True)
+    read_size = 0
+
+    with open(filename, 'wb') as f:
+        while(read_size < size):
+            data = recv_all(sock)
+            f.write(data)
+            read_size += len(data)
+            progress_bar(read_size, size)
+
+def send_size(data, sock):
+    if type(data) is str:
+        data = bytes(data, 'utf8')
+
+    len_str = "{}".format(len(data))
+    len_str_size = chr(len(len_str))
+    send_data = bytes(len_str_size+len_str, 'ascii')+data
+    sock.sendall(send_data)
+
+def client_req(ip, port, message):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((ip, port))
+        send_size(message, sock)
+        response = recv_all(sock)
+        sock.close()
+        return response
+
+def metadata_req(ip, port):
+    global metadata_req_message
+    return client_req(ip, port, metadata_req_message)
+
+def push_update(ip, port, filename):
+    global prepare_message
+    global prepare_response
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((ip, port))
+        send_size(prepare_message, sock)
+        response = recv_all(sock)
+        if response != prepare_response:
+            print("yoohoo: NO NO NO NO")
+            return False;
+        else:
+            send_file(filename, sock)
+            recv_file('.blackjay/s2c.zip', sock)
+
+        sock.close()
--- server.py
+++ server.py
@@ -1,108 +1,58 @@
 import threading
-import socket
 import socketserver
 import time
 import sys
-import struct
 import json
+import shutil
+from zipfile import ZipFile
+
 from metadata import *
-from archiver import *
+from networking import *
 
-metadata_req_message = b"gimme the metadata"
-prepare_message = b"prepare to receive all my acorns"
-prepare_response = b"send your worst"
+def extract_client_to_server_archive(zipfile,UID):
+    # make the temp directory
+    tempdir = '.blackjay/c2s'+UID
+    os.mkdir(tempdir)
+    # extract the zip file
+    with ZipFile(zipfile,'r') as z:
+        z.extractall(tempdir)
+    push = load_metadata(os.path.join(tempdir,'.blackjay/push'))
+    pull = load_metadata(os.path.join(tempdir,'.blackjay/pull'))
+    conflicts = load_metadata(os.path.join(tempdir,'.blackjay/conflicts'))
+    return push, pull, conflicts
 
-def recv_all(sock):
-    total_len = 0; payload_data = b''; size = 0
-    size_data = sock_data = b''
-    len_data = b''; len_size = 0
-    # get the length field size
-    while not len_size:
-        sock_data = sock.recv(1)
-        if len(sock_data)>0:
-            len_size = sock_data[0]
+# now (push, pull, conflicts) are just those changes accepted by the server
+def prep_server_to_client_archive(push, pull, conflicts, UID):
+    # make a response directory
+    zipname = '.blackjay/s2c'+UID+'.zip'
+    # now prep the zip archive
+    with ZipFile(zipname,'w') as z:
+        z.writestr('.blackjay/push',json.dumps(push,indent='    '))
+        z.writestr('.blackjay/pull',json.dumps(pull,indent='    '))
+        z.writestr('.blackjay/conflicts',json.dumps(conflicts,indent='    '))
+        for name,meta in pull.items():
+            if meta['del_flag'] is False:
+                z.write(name)
+        for name,meta in conflicts.items():
+            if meta['del_flag'] is False:
+                z.write(name)
+    return zipname
 
-    # get the length field
-    while len(len_data) < len_size:
-        len_data += sock.recv(len_size-len(len_data))
-        if len(len_data) == len_size:
-            size = int(len_data[:len_size])
+def make_server_updates_live(push,UID):
+    local_meta = load_metadata('.blackjay/metadata')
+    # for pushes which were accepted, update local metadata
+    for name,meta in push.items():
+        local_meta[name] = meta
+        if meta['del_flag'] is False:
+            os.rename(os.path.join('.blackjay/c2s'+UID,name),name)
+    # for pulling from the server, no metadata changes
+    # the server doesn't handle conflicts
+    write_metadata(local_meta,'.blackjay/metadata')
 
-    # get the data
-    while len(payload_data) < size:
-        payload_data += sock.recv(min(size-total_len, 2048))
-
-    return payload_data
-
-def send_file(filename, sock):
-    size = str(os.stat(filename).st_size)
-    print("send_file: file size: {}".format(size), flush=True)
-    with open(filename, 'rb') as f:
-        send_size(size, sock)
-        data = f.read(2048)
-        while data:
-            send_size(data, sock)
-            data = f.read(2048)
-
-def progress_bar(completed, size):
-    width = 60
-    progress = completed/size
-    progress_bar_width = width - 13
-    num_equals = int(progress*progress_bar_width)
-    num_space = int(progress_bar_width - num_equals)
-    progress_bar = "="*num_equals + " "*num_space
-    print("\r[{}] {:.2f}%".format(progress_bar, (progress*100)), end='')
-    if completed == size:
-        print("")
-
-def recv_file(filename, sock):
-    size = int(recv_all(sock))
-    print("recv_file: {}, size: {}".format(filename,size), flush=True)
-    read_size = 0
-
-    with open(filename, 'wb') as f:
-        while(read_size < size):
-            data = recv_all(sock)
-            f.write(data)
-            read_size += len(data)
-            progress_bar(read_size, size)
-
-def send_size(data, sock):
-    if type(data) is str:
-        data = bytes(data, 'utf8')
-
-    len_str = "{}".format(len(data))
-    len_str_size = chr(len(len_str))
-    send_data = bytes(len_str_size+len_str, 'ascii')+data
-    sock.sendall(send_data)
-
-def client_req(ip, port, message):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((ip, port))
-        send_size(message, sock)
-        response = recv_all(sock)
-        sock.close()
-        return response
-
-def metadata_req(ip, port):
-    global metadata_req_message
-    return client_req(ip, port, metadata_req_message)
-
-def push_update(ip, port, filename):
-    global prepare_message
-    global prepare_response
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((ip, port))
-        send_size(prepare_message, sock)
-        response = recv_all(sock)
-        if response != prepare_response:
-            print("yoohoo: NO NO NO NO")
-            return False;
-        else:
-            send_file(filename, sock)
-            recv_file('.blackjay/s2c.zip', sock)
-
-        sock.close()
+def cleanup_server_temp_files(UID):
+    os.remove('.blackjay/c2s'+UID+'.zip')
+    os.remove('.blackjay/s2c'+UID+'.zip')
+    shutil.rmtree('.blackjay/c2s'+UID+'')
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
